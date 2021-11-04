69c35ba22e922cf46404c95456082d06fcfbb10d
WeMayNeverKnow <77531972+WeMayNeverKnow@users.noreply.github.com>
2021-11-04 16:29:15 +0800

    added file structure

---
+++ main/resources/student.py
@@ -0,0 +1,16 @@
+from flask_restful import Resource
+
+
+class Student(Resource):
+    def get(self):
+        pass
+
+    def put(self):
+        pass
+
+    def post(self):
+        pass
+
+    def delete(self):
+        pass
+
