6fe346c7cfeb17c0c23d0c2236b6e968c00f84c0
WeMayNeverKnow <77531972+WeMayNeverKnow@users.noreply.github.com>
2021-11-11 14:49:32 +0800

    built better structure

--- main/app.py
+++ main/app.py
@@ -1,10 +1,39 @@
 from flask import Flask
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 
+from resources.people import People
+from resources.sections import Sections
 from resources.students import Students
 
-app = Flask(__name__)
-api = Api(app)
+import os
 
-api.add_resource(Students)
+
+# Must have something in LOCAL (.env) for testing
+IS_LOCAL = True if os.getenv('LOCAL') is not None else False
+
+if DATABASE_URI := os.getenv('DATABASE') is None:
+    DATABASE_URI = 'sqlite:///database.db'
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+
+api = Api(app)
+db = SQLAlchemy(app)
+
+
+def add_resource(api):
+    api.add_resource(People)
+    api.add_resource(Students)
+    api.add_resource(Sections)
+
+
+add_resource(api)
+# db.create_all()
+
+
+if __name__ == '__main__':
+    app.run(debug=IS_LOCAL)
 
---
+++ main/resources/people.py
@@ -0,0 +1,15 @@
+from flask_restful import Resource
+
+class People(Resource):
+    def get(self):
+        pass
+
+    def put(self):
+        pass
+
+    def post(self):
+        pass
+
+    def delete(self):
+        pass
+
