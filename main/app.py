bd7637a6bfec798104c51e1ebb219f8a87b88459
WeMayNeverKnow <77531972+WeMayNeverKnow@users.noreply.github.com>
2021-11-23 12:03:05 +0800

    idk will comeback soonTM

--- main/app.py
+++ main/app.py
@@ -2,34 +2,25 @@ import os
 
 from flask import Flask
 from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, event
 
 from .resources.people import People
 from .resources.sections import Sections
 from .resources.students import Students
 
 
-if DATABASE_URI := os.getenv('DATABASE') is None:
-    DATABASE_URI = 'sqlite:///database.db'
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 
 api = Api(app)
 db = SQLAlchemy(app)
-
-
-def create_db(db):
-    from main.models import Person, Section, Student
-    db.create_all()
+db.create_all()
 
 def add_resource(api):
     api.add_resource(People)
     api.add_resource(Students)
     api.add_resource(Sections)
 
-
 add_resource(api)
-create_db(db)
 
--- main/models.py
+++ main/models.py
@@ -2,38 +2,50 @@ from main.app import db
 from datetime import datetime
 
 
-class Person(db.Model):
-    person_id  = db.Column(db.Integer, primary_key = True)
-    first_name = db.Column(db.String(69), nullable = False)
-    last_name  = db.Column(db.String(69), nullable = False)
-    gender     = db.Column(db.String(1))
-    birth_date = db.Column(db.DateTime)
+clas Person(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    
+    first_name   = db.Column(db.String(69), nullable = False)
+    last_name    = db.Column(db.String(69), nullable = False)
+    gender       = db.Column(db.String(1))
+    birth_date   = db.Column(db.DateTime)
     phone_number = db.Column(db.Integer)
     email        = db.Column(db.String(69))
-    date_added = db.Column(db.DateTime, default = datetime.utcnow,
-            nullable = False)
+    date_added   = db.Column(db.DateTime, default = datetime.utcnow)
+    
+    db.relationship('Student')
     
     def __repr__(self):
-        return f"Person('{self.first_name}', '{self.last_name}')"
+        return f"Person({self.id=}, {self.first_name=}, {self.last_name=})"
 
 
 class Section(db.Model):
-    section_id   = db.Column(db.Integer,    primary_key = True)
-    section_name = db.Column(db.String(69), nullable = False, unique = True)
-    mayor_id     = db.Column(db.Integer, db.ForeignKey('student.student_id'))
-    vice_mayor_id = db.Column(db.Integer, db.ForeignKey('student.student_id'))
-    students = db.relationship('Student', backref = 'section', lazy = True)
+    id = db.Column(db.Integer, primary_key = True)
+
+    section_name  = db.Column(db.String(69), nullable = False, unique = True)
+    mayor_id      = db.Column(db.Integer, db.ForeignKey('student.id'))
+    vice_mayor_id = db.Column(db.Integer, db.ForeignKey('student.id'))
+    secretary_id  = db.Column(db.Integer, db.ForeignKey('student.id'))
+    treasurer_id  = db.Column(db.Integer, db.ForeignKey('student.id'))
+    auditor_id    = db.Column(db.Integer, db.ForeignKey('student.id'))
+    pro_id        = db.Column(db.Integer, db.ForeignKey('student.id'))
+    
+    students = db.relationship('Student')
+
 
     def __repr__(self):
-        return f"Section('{self.section_name}'')"
+        return f"Section({self.id=}, {self.section_name=})"
 
 
 class Student(db.Model):
-    student_id = db.Column(db.Integer, db.ForeignKey('person.person_id'),
-            nullable = False, unique = True)
-    section_id = db.Column(db.Integer. db.ForeignKey('section.section_id'),
+    id = db.Column(db.Integer, db.ForeignKey('person.id'),
+            nullable = False, primary_key = True)
+
+    section_id = db.Column(db.Integer, db.ForeignKey('section.id'),
             nullable = False)
+    
+    db.relationship('Section')
 
     def __repr__(self):
-        return f"Student('{self.student_id}', '{self.section_id}')"
+        return f"Student({self.id=})"
 
